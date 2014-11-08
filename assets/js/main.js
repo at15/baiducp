5cd221476c63c0d176857e703181f62b4f26cf2a
at15 <at15@outlook.com>
2014-11-08 23:17:22 +0800

    Add bkg and random bkg
    also fix some small problems

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,22 @@
 /**
  * Created by at15_000 on 2014/11/8.
  */
+init();
+
+function init(){
+    changeBkg();
+}
 
 // try to use native selector, just for fun :)
 function submit() {
     var form = document.getElementById('bd-form');
     form.submit();
+}
+
+// change bkg randomly
+function changeBkg(){
+    var amount = 8;
+    var r = Math.floor(Math.random()*amount);
+    var bkgSrc = 'assets/images/bkg' + r + '.jpg';
+    document.getElementById('bkg-img').setAttribute('src',bkgSrc);
 }

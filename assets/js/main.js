a4608491379f343b03c82064b83a576144be11e3
at15 <at15@outlook.com>
2014-11-08 19:58:46 +0800

    Add fontawesome and finish the search bar
    also the footer. need to deal with nav now

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by at15_000 on 2014/11/8.
+ */
+
+// try to use native selector, just for fun :)
+function submit() {
+    var form = document.getElementById('bd-form');
+    form.submit();
+}
